40da422ce489d236a2be26282d96e0f3611f1b18
robertBeattie <rbeattie@uvm.edu>
2020-04-18 15:52:47 -0400

    Lantern

--- Assets/Scripts/lanternMovement.cs
+++ Assets/Scripts/lanternMovement.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class lanternMovement : MonoBehaviour
 {
-
+    [Header("Follow Player")]
     [SerializeField] GameObject player;
     [SerializeField] Vector3 offset;
-    [SerializeField] float delay = .5f;
     [SerializeField] float speed = 5;
-    
+
+    [Header("Follow Mouse")]
+    [SerializeField] float delay = 5;
+
 
     // Update is called once per frame
     void Update() 
     {
-        Vector3 playerPos = player.transform.position * -player.transform.localScale.x;
+        FollowMouse();
+    }
+    void FollowPlayer()
+    {
+        Vector3 offset = new Vector3(this.offset.x * -player.transform.localScale.x, this.offset.y, 0);
+      
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, Time.deltaTime * speed);
+    }
+    void FollowMouse()
+    {
+        Vector3 mosPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        mosPos.z = 0;
 
-        transform.position = Vector3.Lerp(transform.position, playerPos + offset, Time.deltaTime * speed);
+        transform.position = Vector3.Lerp(mosPos, transform.position, delay * Time.deltaTime);
     }
 }
