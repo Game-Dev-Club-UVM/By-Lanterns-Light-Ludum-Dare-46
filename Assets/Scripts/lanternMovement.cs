5e558e259d4f63617056205986c490c149d3b00a
Alex Silence <asilence@uvm.edu>
2020-04-18 14:14:07 -0400

    Started lantern

---
+++ Assets/Scripts/lanternMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lanternMovement : MonoBehaviour
+{
+
+    [SerializeField] GameObject player;
+    [SerializeField] Vector3 offset;
+    [SerializeField] float delay = .5f;
+    [SerializeField] float speed = 5;
+    
+
+    // Update is called once per frame
+    void Update() 
+    {
+        Vector3 playerPos = player.transform.position * -player.transform.localScale.x;
+
+        transform.position = Vector3.Lerp(transform.position, playerPos + offset, Time.deltaTime * speed);
+    }
+}
