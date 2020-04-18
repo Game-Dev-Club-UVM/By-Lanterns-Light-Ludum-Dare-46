2a790cf0de8cd6faebf8252d7463adcb8504b5a3
John Urbani <35612888+JohnUrbani@users.noreply.github.com>
2020-04-18 16:18:56 -0400

    required update

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(PlayerMovementGravityChange))]
 public class CharacterController2D : MonoBehaviour
 {
 	[SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
