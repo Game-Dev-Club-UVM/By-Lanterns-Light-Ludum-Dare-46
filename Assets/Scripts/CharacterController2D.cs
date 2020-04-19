9831ee6fe79aa927bb203b2019626f1a3e72fd3f
John Urbani <35612888+JohnUrbani@users.noreply.github.com>
2020-04-19 14:25:20 -0400

    Add jumping support

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -34,6 +34,11 @@ public class CharacterController2D : MonoBehaviour
 	public BoolEvent OnCrouchEvent;
 	private bool m_wasCrouching = false;
 
+	public bool getGrounded()
+	{
+		return m_Grounded;
+	}
+
 	private void Awake()
 	{
 		m_Rigidbody2D = GetComponent<Rigidbody2D>();
--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -5,56 +5,78 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     Animator animator;
+    bool onGround = true;
+    CharacterController2D charController;
+
+    public void touchGround()
+    {
+        onGround = true;
+        animator.SetBool("onGround", true);
+    }
 
     void Awake()
     {
         animator = GetComponent<Animator>();
+        charController = GetComponentInParent<CharacterController2D>();
     }
 
     void Update()
     {
+        // Check if on ground
+        onGround = charController.getGrounded();
+        animator.SetBool("onGround", onGround);
+
         // Run animations
-        if (Input.GetAxis("Horizontal") != 0 && !Input.GetKey(KeyCode.LeftControl))
+        if (Input.GetAxis("Horizontal") != 0 && !Input.GetKey(KeyCode.LeftControl) && onGround)
         {
             animator.SetBool("isCrouching", false);
             animator.SetBool("isWalking", false);
             animator.SetBool("isRunning", true);
         } 
-        else if (Input.GetAxis("Horizontal") != 0 && Input.GetKey(KeyCode.LeftControl))
+        else if (Input.GetAxis("Horizontal") != 0 && Input.GetKey(KeyCode.LeftControl) && onGround)
         {
             animator.SetBool("isCrouching", true);
             animator.SetBool("isWalking", false);
             animator.SetBool("isRunning", true);
         }
         // Walk animation transistions
-        else if (Input.GetAxis("Horizontal") == 0 && !Input.GetKey(KeyCode.LeftControl)) {
+        else if (Input.GetAxis("Horizontal") == 0 && !Input.GetKey(KeyCode.LeftControl) && onGround) {
             animator.SetBool("isCrouching", false);
             animator.SetBool("isRunning", false);
             animator.SetBool("isWalking", true);
         }
-        else if (Input.GetAxis("Horizontal") == 0 && Input.GetKey(KeyCode.LeftControl))
+        else if (Input.GetAxis("Horizontal") == 0 && Input.GetKey(KeyCode.LeftControl) && onGround)
         {
             animator.SetBool("isCrouching", true);
             animator.SetBool("isRunning", false);
             animator.SetBool("isWalking", true);
         }
         // Idle animations
-        else if (!Input.GetKey(KeyCode.LeftControl))
+        else if (!Input.GetKey(KeyCode.LeftControl) && onGround)
         {
             animator.SetBool("isCrouching", false);
             animator.SetBool("isWalking", false);
             animator.SetBool("isRunning", false);
         }
-        else
+        else if(Input.GetKey(KeyCode.LeftControl) && onGround)
         {
             animator.SetBool("isCrouching", true);
             animator.SetBool("isWalking", false);
             animator.SetBool("isRunning", false);
-        }
-
-        if (Input.GetButtonDown("Jump"))
+        } 
+        // Jumping trigger animations
+        else if (Input.GetButtonDown("Jump") && onGround)
         {
-            animator.SetTrigger("jump");
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                animator.SetTrigger("powerJump");
+            }
+            else
+            {
+                animator.SetTrigger("jump");
+            }
+            onGround = false;
+            animator.SetBool("onGround", false);
         }
     }
 }
