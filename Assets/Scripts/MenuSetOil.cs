a6f131d8c45fe24e40ccbc7fc7db6d95537415ca
robertBeattie <rbeattie@uvm.edu>
2020-04-20 12:55:40 -0400

    sol flames works and added

---
+++ Assets/Scripts/MenuSetOil.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSetOil : MonoBehaviour
+{
+    [SerializeField] OilData oilData;
+    public void setUpOilData()
+    {
+        oilData.maxOil = 100;
+        oilData.currentOil = 100;
+    }
+}
