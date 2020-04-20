2c6f7f57f9324ec6db05954fbda195e804cb28c7
John Urbani <35612888+JohnUrbani@users.noreply.github.com>
2020-04-20 19:48:56 -0400

    lantern death

--- Assets/Scripts/Oil.cs
+++ Assets/Scripts/Oil.cs
@@ -2,24 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Experimental.Rendering.Universal;
 public class Oil : MonoBehaviour
 {
     [SerializeField] OilData oilData;
     [SerializeField] OilBar oilBar;
     LevelManager levelManager;
+    GameObject lantern;
+    Light2D lanternLight;
+    [SerializeField] float timeSpent = 0;
+    [SerializeField] float deathTime = 3;
+    [SerializeField] float radiusNormal = 4;
+    [SerializeField] float radiusDeath = 100;
+    [SerializeField] float intensityNormal = 1;
+    [SerializeField] float intensityDeath = 8;
+    [SerializeField] float timeSpent2 = 0;
+    [SerializeField] float deathTime2 = 1;
+    bool explode = false;
     private void Start()
     {
         oilBar = GameObject.FindGameObjectWithTag("OilBar").GetComponent<OilBar>();
         oilBar.SetMaxHealth(oilData.maxOil);
         oilBar.SetHealth(oilData.currentOil);
         levelManager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
+        lantern = GameObject.Find("Lantern");
+        lanternLight = lantern.GetComponentInChildren<Light2D>();
     }
     public bool isOutOfOil()
     {
-        if(oilData.currentOil <= 0)
+        if(oilData.currentOil <= 0 && !explode)
         {
-            levelManager.ReloadScene();
-            setOil(oilData.maxOil);
+            //levelManager.ReloadScene();
+            explode = true;
+            StartCoroutine(resetLantern());
+            //setOil(oilData.maxOil);
+            //timeSpent = 0;
             return true;
         }
         else
@@ -27,6 +44,44 @@ public class Oil : MonoBehaviour
             return false;
         }
     }
+
+    public void Update()
+    {
+        if (explode && timeSpent < deathTime)
+        {
+            timeSpent += Time.deltaTime;
+            lanternLight.pointLightOuterRadius = Mathf.Lerp(radiusNormal, radiusDeath, timeSpent / deathTime);
+            lanternLight.intensity = Mathf.Lerp(intensityNormal, intensityDeath, timeSpent / deathTime);
+        } 
+        else if (lanternLight.pointLightOuterRadius != radiusNormal)
+        {
+            timeSpent2 += Time.deltaTime;
+            lanternLight.pointLightOuterRadius = Mathf.Lerp(radiusDeath, radiusNormal, timeSpent2 / deathTime2);
+            lanternLight.intensity = Mathf.Lerp(intensityDeath, intensityNormal, timeSpent2 / deathTime2);
+        } 
+        else
+        {
+            timeSpent2 = 0;
+            lanternLight.pointLightOuterRadius = radiusNormal;
+            lanternLight.intensity = intensityNormal;
+        }
+    }
+
+    IEnumerator resetLantern()
+    {
+        yield return new WaitForSeconds(deathTime);
+
+        levelManager.ReloadScene();
+        setOil(oilData.maxOil);
+        timeSpent = 0;
+        explode = false;
+
+        //yield return new WaitForSeconds(deathTime);
+
+        //lanternLight.pointLightOuterRadius = radiusNormal;
+        //lanternLight.intensity = intensityNormal;
+    }
+
     public void setOil(float amount)
     {
         if(amount > oilData.maxOil)
