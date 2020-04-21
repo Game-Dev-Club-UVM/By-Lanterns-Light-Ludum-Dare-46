eccfd4c48974c6636e59c63a734a7f09d4a02cb7
robertBeattie <rbeattie@uvm.edu>
2020-04-20 20:59:29 -0400

    sounds and witch

---
+++ Assets/StartWitch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartWitch : MonoBehaviour
+{
+   [SerializeField] private WitchController witch;
+    bool active = false;
+    private void Awake()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ( !active && collision.tag == "Player")
+        {
+            witch.alive = true;
+            witch.witchChasing.enabled = true;
+            active = true;
+        }
+    }
+}
---
+++ Assets/WitchController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitchController : MonoBehaviour
+{
+    [SerializeField] Vector3 startPos;
+    [SerializeField] private Oil lantern;
+    private AudioSource audioSource;
+
+    [SerializeField] private AudioClip clip1;
+    [SerializeField] private AudioClip clip2;
+    private bool played = false;
+
+    private float laughTimer = 4f;
+    private float laughCount = 0f;
+
+    public bool alive = false;
+
+    [SerializeField] public WitchChasing witchChasing;
+    private void Awake()
+    {
+        startPos = transform.position;
+        audioSource = GetComponent<AudioSource>();
+        lantern = GameObject.FindGameObjectWithTag("Lantern").GetComponent<Oil>();
+
+    }
+
+    private void Update()
+    {
+        if(lantern.getOil() <= 0)
+        {
+            transform.position = startPos;
+        }
+        if (!alive) { return; }
+        if (laughCount <= 0)
+        {
+            audioSource.Play();
+            laughCount = laughTimer;
+            played = true;
+        }
+        else
+        {
+            laughCount -= Time.deltaTime;
+        }
+        if (!audioSource.isPlaying && played)
+        {
+            if(Random.Range(0,100) % 2 == 0)
+            {
+                audioSource.clip = clip1;
+            }
+            else
+            {
+                audioSource.clip = clip2;
+            }
+            
+            played = false;
+        }
+    }
+}
