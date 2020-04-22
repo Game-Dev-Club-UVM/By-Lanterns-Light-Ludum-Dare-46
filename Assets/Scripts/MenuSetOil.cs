d6821d445591a352189e1dce8fea4c054cc6a86d
robertBeattie <rbeattie@uvm.edu>
2020-04-22 09:29:35 -0400

    build 1.5

--- Assets/Scripts/MenuSetOil.cs
+++ Assets/Scripts/MenuSetOil.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class MenuSetOil : MonoBehaviour
 {
     [SerializeField] OilData oilData;
+
+    private void Start()
+    {
+        Cursor.visible = true;
+    }
     public void setUpOilData()
     {
         oilData.maxOil = 100;
